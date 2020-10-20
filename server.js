4bcf3d94c498cedd1e60834e028f4603084e9245
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-20 12:35:25 -0700

    Added function viewAll to view the list of all employees

--- server.js
+++ server.js
@@ -59,3 +59,12 @@ function start() {
         };
     });
 };
+
+function viewAll() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.department, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", function (err, res) {
+        if (err) throw err
+
+        console.table(res)
+        start()
+    });
+};
