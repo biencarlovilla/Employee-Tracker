ce94f2f8d74af7b44c07206deba0f72a94159474
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-20 12:01:06 -0700

    Created connection for the database

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 var mysql = require("mysql")
 var inquirer = require("inquirer")
 var consoleTable = require("console.table")
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employeeDB"
+})
