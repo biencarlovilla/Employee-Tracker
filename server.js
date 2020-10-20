df09c595030ea8547e61dc1ea1e66a5841b2348d
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-20 12:31:03 -0700

    added functions for user interface

--- server.js
+++ server.js
@@ -15,3 +15,47 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
     start()
 });
+
+function start() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "action",
+            choices: ["View All Employees", "View All Employees By Department", "View All Employees by Role", "Add Employee", "Add Role", "Add Department", "Update Employee Role", "Exit"]
+        }
+    ]).then(function (res) {
+        switch (res.action) {
+            case "View All Employees":
+                viewAll();
+                break;
+
+            case "View All Employees By Department":
+                viewDept();
+                break;
+
+            case "View All Employees by Role":
+                viewRole();
+                break;
+
+            case "Add Employee":
+                addEmployee();
+                break;
+
+            case "Add Role":
+                addRole();
+                break;
+
+            case "Add Department":
+                addDept();
+                break;
+
+            case "Update Employee Role":
+                updateEmployee();
+                break;
+
+            case "Exit":
+                return;
+        };
+    });
+};
