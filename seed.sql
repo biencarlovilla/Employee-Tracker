93e93a2cbdef79b92534788e399af4061439f3d3
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-20 14:53:57 -0700

    I ran npm installs on mysql, content.table

--- seed.sql
+++ seed.sql
@@ -4,14 +4,9 @@ CREATE DATABASE employeeDB;
 
 USE employeeDB;
 
-CREATE TABLE employee (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(40),
-  last_name VARCHAR(40),
-  manager_id INT,
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  department VARCHAR(40)
 );
 
 CREATE TABLE role (
@@ -22,28 +17,24 @@ CREATE TABLE role (
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department VARCHAR(40)
+  first_name VARCHAR(40),
+  last_name VARCHAR(40),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
-
-
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Tony", "Stark", 1, 1);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Bruce", "Banner", 4, 2);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Steve","Rogers", 2, 3);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Natasha", "Romanoff", null, 4);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Clinton", "Barton", null, 5);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Thor", "OfAsgard", 1, 6);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Peter", "Parker", null, 7);
-
+INSERT INTO department (department)
+VALUE ("Engineering");
+INSERT INTO department (department)
+VALUE ("Administrator");
+INSERT INTO department (department)
+VALUE ("Event Coordinator");
+INSERT INTO department (department)
+VALUE ("Finance");
 
 INSERT INTO role (title, salary, department_id)
 VALUE ("Software Engineer", 100000, 1);
@@ -60,12 +51,17 @@ VALUE ("Quality Assurance", 200000, 1);
 INSERT INTO role (title, salary, department_id)
 VALUE ("Accountant", 160000, 4);
 
-
-INSERT INTO department (department)
-VALUE ("Engineering");
-INSERT INTO department (department)
-VALUE ("Administrator");
-INSERT INTO department (department)
-VALUE ("Event Coordinator");
-INSERT INTO department (department)
-VALUE ("Finance");
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Tony", "Stark", 1, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Bruce", "Banner", 1, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Steve","Rogers", null, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Natasha", "Romanoff", null, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Clinton", "Barton", null, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Thor", "OfAsgard", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Peter", "Parker", 4, 7);
