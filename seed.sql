d0293182b6afd7527d06de3f496f170d50ccb62c
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-20 10:44:28 -0700

    Created table employee and schemas into employeeDB

--- seed.sql
+++ seed.sql
@@ -4,3 +4,10 @@ CREATE DATABASE employeeDB;
 
 USE employeeDB;
 
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(40),
+  last_name VARCHAR(40),
+  manager_id INT,
+  role_id INT,
+);
